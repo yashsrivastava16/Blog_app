507061e3c8dd4b407f8f0654e6b1190d2184bc93
YashSrivastava <yashsrivastavaa16@gmail.com>
2022-03-02 10:04:53 +0530

    gitignore file added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Blog App
+          Blog App with mern
         </a>
       </header>
     </div>
