cc253f67af9b3453794a8c2111dbb6ec800dc9c2
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2022-03-03 14:48:00 +0530

    added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,38 @@
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import Signup from "./pages/signup/Signup";
+import Login from './pages/login/Login'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/post/:postId">
+            <Single />
+          </Route>
+          <Route path="/write">
+            <Write />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
