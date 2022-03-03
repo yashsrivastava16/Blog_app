e32ce5c6c26c0f0ab9b5b67b0cc98ef753799e25
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2022-03-03 14:00:56 +0530

    fixed login page css

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -5,10 +5,14 @@ const Login = () => {
     <div className='login'>
       <span className="loginTitle">Login</span>
       <form action="" className="loginForm">
-        <label htmlFor="email">E-mail</label>
-        <input className='loginInput' type="email" name="email" id="email" placeholder="Enter your E-mail" />
-        <label htmlFor="password">Password</label>
-        <input className='loginInput' type="password" name="password" id="password" placeholder="Enter your Password..." />
+        <div className="loginGroup">
+          <label htmlFor="email">E-mail</label>
+          <input className='loginInput' type="email" name="email" id="email" placeholder="Enter your E-mail" />
+        </div>
+        <div className="loginGroup">
+          <label htmlFor="password">Password</label>
+          <input className='loginInput' type="password" name="password" id="password" placeholder="Enter your Password..." />
+        </div>
         <button className='loginButton'>Login</button>
       </form>
       <span className='loginOr'>OR</span>
