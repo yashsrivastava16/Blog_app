cf5131d03bd5e088037faa43e45e6bec9db2e54d
YashSrivastava <yashsrivastavaa16@gmail.com>
2022-03-04 13:22:27 +0530

    bug fix

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -13,7 +13,7 @@ router.post("/register",async(req,res)=>{
         const user = await newUser.save();
         res.status(200).json(user)
     } catch (error) {
-        res.status(500).json(`Something went wrong please try again with unique username and password!!`);
+        res.status(500).json(`Something went wrong please try again with unique emailID and password!!`);
     }
 });
 
