35ae599a4b6ebe31ffbd74bf2137ae9f47c5632d
YashSrivastava <yashsrivastavaa16@gmail.com>
2022-03-11 13:46:52 +0530

    context added

---
+++ frontend/src/context/Action.js
@@ -0,0 +1,12 @@
+export const LoginStart = (userCredentials)=>({
+    type:"LOGIN_START"
+})
+
+export const LoginSuccess = (user)=>({
+    type:"LOGIN_SUCCESS",
+    playload:user
+});
+
+export const LoginFaliure = ()=>({
+    type : "LOGIN_FALIURE"
+})
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,20 +1,41 @@
 import { Link } from 'react-router-dom';
+import {useContext, useRef } from "react";
 import './login.css';
+import { Context } from '../../context/Context';
+import axios from 'axios';
 
 const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const {dispatch,user} = useContext(Context)
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    dispatch({type:"LoginStart"})
+    try {
+      const res = await axios.post("/auth/login",{
+        email : emailRef.current.value,
+        password : passwordRef.current.value,
+      });
+    dispatch({type:"LoginSuccess",payload:res.data})
+    } catch (error) {
+    dispatch({type:"LoginFaliure"})
+    }
+  }
+  console.log(user);
   return (
     <div className='login'>
       <span className="loginTitle">Login</span>
-      <form action="" className="loginForm">
+      <form action="" className="loginForm" onSubmit={handleSubmit}>
         <div className="loginGroup">
           <label htmlFor="email">E-mail</label>
-          <input className='loginInput' type="email" name="email" id="email" placeholder="Enter your E-mail" />
+          <input className='loginInput' type="email" name="email" id="email" placeholder="Enter your E-mail" ref={emailRef} />
         </div>
         <div className="loginGroup">
           <label htmlFor="password">Password</label>
-          <input className='loginInput' type="password" name="password" id="password" placeholder="Enter your Password..." />
+          <input className='loginInput' type="password" name="password" id="password" placeholder="Enter your Password..." ref={passwordRef} />
         </div>
-        <button className='loginButton'>Login</button>
+        <button className='loginButton' type="submit">Login</button>
       </form>
       <span className='loginOr'>OR</span>
       <button className='loginSignupButton'>
